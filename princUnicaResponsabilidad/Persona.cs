3b5409f2636d5d65ea093a998de8174cab06665d
Paula Henrsez <paulahenrsez@gmail.com>
2024-07-18 18:24:50 -0400

    Agregar archivos de proyecto.

---
+++ princUnicaResponsabilidad/Persona.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace princUnicaResponsabilidad
+{
+    public class Persona
+    {
+        public string nombre;
+        public int edad;
+        public string direccion;
+        public string correoElectronico;
+    }
+
+    public class EmailService
+    {
+        public void EnviarCorreoElectronico(string mensaje)
+        {
+            // Lógica para enviar correo electrónico
+        }
+    }
+
+    public class ImprimirPersona
+    {
+        private Persona persona;
+
+        public ImprimirPersona(Persona persona)
+        {
+            this.persona = persona;
+        }
+        public void ImprimirDatos()
+        {
+            Console.WriteLine($"Nombre: {persona.nombre}");
+            Console.WriteLine($"Edad: {persona.edad}");
+            Console.WriteLine($"Dirección: {persona.direccion}");
+            Console.WriteLine($"Correo electrónico: {persona.correoElectronico}");
+        }
+    }
+
+}
